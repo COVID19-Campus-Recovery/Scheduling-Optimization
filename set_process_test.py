66fc92c683164a62af764c87543a27aa33192345
Mehran Navabi <mehrnavabi@gmail.com>
2020-06-20 16:45:33 -0700

    begin tests for set_process

---
+++ set_process_test.py
@@ -0,0 +1,51 @@
+import unittest
+import set_process as sp
+
+def instantiate_room_assignment_model():
+
+    course_data_filepath = "~/Documents/Madgie_Cleaned_Directory/Data/room_assignment_opt_courses_example.xlsx"
+    room_data_filepath = "~/Documents/Madgie_Cleaned_Directory/Data/room_assignment_opt_rooms_example.xlsx"
+    course_data = dp.clean_course_data(course_data_filepath)
+    room_data = dp.clean_room_data(room_data_filepath)
+    assign_opt = RoomAssignmentOpt(course_data, room_data)
+    return assign_opt
+
+class TestSetProcesses(unittest.TestCase):
+
+
+	def test_get_overlapping_time_slots_time_overlap(self):
+
+		all_timeslot = ['MWF_800_900', 'MWF_900_1000','MWF_1000_1100', 'MWF_1100_1200', 'MWF_1200_1300', 'MWF_1300_1400', 'MWF_1400_1500', 'MWF_1500_1600']
+		
+		#test single time 
+		self.assertEqual(sp.get_overlapping_time_slots(all_timeslot, 'MWF_1300_1400'), {'MWF_1300_1400'})
+
+		#no overlap
+		self.assertEqual(sp.get_overlapping_time_slots(all_timeslot, 'MWF_600_700'), set())
+
+		#multiple overlap, between times. Should only return earlier time
+		self.assertEqual(sp.get_overlapping_time_slots(all_timeslot, 'MWF_1230_1330'), {'MWF_1200_1300'})
+
+		#multiple overlap, long timeframe
+		# self.assertEqual(sp.get_overlapping_time_slots(all_timeslot, 'MWF_900_1300'), {'MWF_900_1000','MWF_1000_1100', 'MWF_1100_1200', 'MWF_1200_1300'})
+
+		self.assertEqual(sp.get_overlapping_time_slots(['MWF_900_1300'], 'MWF_1000_1100'), {'MWF_900_1300'})
+
+	def test_get_overlapping_time_slots_dow_overlapt(self):
+
+		all_timeslot = ['MWF_800_900', 'M_800_900', 'W_800_900', 'F_800_900', 'T_800_900', 'R_800_900']
+
+		#if timeslot is includes multiple days, then timeslots including any of those days should be returned
+		self.assertEqual(sp.get_overlapping_time_slots(all_timeslot, 'MWF_800_900'), {'MWF_800_900', 'M_800_900','W_800_900', 'F_800_900'})
+		self.assertEqual(sp.get_overlapping_time_slots(all_timeslot, 'TR_800_900'), {'TR_800_900', 'T_800_900','R_800_900'})
+
+		#if timeslot includes only one day, only the same day should be returned 
+		self.assertEqual(sp.get_overlapping_time_slots(all_timeslot, 'M_800_900'), {'M_800_900', 'MWF_800_900'})
+		self.assertEqual(sp.get_overlapping_time_slots(all_timeslot, 'W_800_900'), {'W_800_900', 'MWF_800_900'})
+		self.assertEqual(sp.get_overlapping_time_slots(all_timeslot, 'F_800_900'), {'F_800_900', 'MWF_800_900'})
+		self.assertEqual(sp.get_overlapping_time_slots(all_timeslot, 'T_800_900'), {'T_800_900', 'TR_800_900'})
+		self.assertEqual(sp.get_overlapping_time_slots(all_timeslot, 'R_800_900'), {'R_800_900', 'TR_800_900'})
+
+
+if __name__ == '__main__':
+    unittest.main()
