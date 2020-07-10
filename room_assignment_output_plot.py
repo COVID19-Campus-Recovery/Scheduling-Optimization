75adca243efea440aaa87bc02cf5205d4388f397
Mohamed <meltonbari@gmail.com>
2020-07-10 13:20:06 -0400

    Added function to make a bar plot representing fraction of satisfied delivery modes by raw preference.

--- room_assignment_output_plot.py
+++ room_assignment_output_plot.py
@@ -1,7 +1,78 @@
 import pandas as pd
+import csv
 import matplotlib.pyplot as plt
 import data_process as dp
-import set_process as sp
+# import set_process as sp
+
+
+def plot_delivery_mode_satisfied(output_filepath):
+	"""
+	Return a bar plot representing fraction of satisfied delievery modes.
+
+	Parameters
+	----------
+	output_filepath : str
+	Filepath to output file.
+
+	Returns
+	-------
+	plt : matplotlib.pyplot module
+	satisfied_count : dict(str, float)
+	Maps raw preference to number of times it is satisfied.
+	total_count : dict(str, float)
+	Maps raw preference to total number of times it appears in the data.
+	"""
+	model_output_column = "delivery_mode"
+	preference_column = "preference"
+	plot_by_column = "raw_preference"
+
+	satisfied_count = {}
+	total_count = {}
+	with open(output_filepath, "r") as file:
+		reader = csv.DictReader(file)
+		for row in reader:
+			model_output = row[model_output_column]
+			preference = row[preference_column]
+			model_output_formatted, preference_formatted = format_delivery_mode(
+			model_output, preference)
+
+			bar_name = row[plot_by_column].strip()
+			if bar_name not in satisfied_count.keys():
+				satisfied_count[bar_name] = 0
+				total_count[bar_name] = 0
+
+			if model_output_formatted in preference_formatted:
+				satisfied_count[bar_name] += 1
+			total_count[bar_name] += 1
+
+	percent_satisfied = {p: satisfied_count[p] / total_count[p]
+	for p in satisfied_count.keys()}
+	plt.bar(percent_satisfied.keys(), percent_satisfied.values())
+	plt.title("Fraction of Satisfied Delivery Modes")
+	return plt, satisfied_count, total_count
+
+
+def format_delivery_mode(*args):
+	"""Format string arguments.
+	
+	Lowercase all letters and remove leading/trailing whitespaces. If the 
+	string is multiple comma-separated words, split the string into a list
+	containing each individual word. 
+
+	"Residential" and "Hybrid" are changed to "residential_spread" and
+	"hybrid_split,hybrid_touchpoint,residential_spread", respectively,
+	to maintain consistency across columns.
+	"""
+	formatted = []
+	for a in args:
+		a.lower().strip()
+		if a == "residential":
+			a += "_spread"
+		if a == "hybrid":
+			a = "hybrid_split,hybrid_touchpoint,residential_spread"
+		f = [s.strip() for s in a.split(",")]
+		formatted.append(f[0] if len(f) == 1 else f)
+	return formatted
 
 
 def room_assignment_output_plot(plot_type, directory, output_filename, title=None, room_use_filter=None, bins=10):
@@ -88,3 +159,11 @@ def count_consistent_assignments(output_filepath_a, output_filepath_b):
 
 	return num_consistent_assignments
 
+
+if __name__ == "__main__":
+	output_filepath = ("/home/mtonbari/Projects/covid_recovery/Output/room_assignment_opt_output_full_enrollment_2020_Class_20_percent_social_distance_preferences_contact_max.csv")
+	plt, satisfied, total = plot_delivery_mode_satisfied(output_filepath)
+	for k in satisfied.keys():
+	    print(k +":", satisfied[k] / total[k])
+	plt.show()
+	print(type(plt))
